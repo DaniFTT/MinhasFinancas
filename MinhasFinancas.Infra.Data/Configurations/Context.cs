81025b68571ca4ab6426320d677c72da896bc84e
Daniel Santos De Sousa <diel-e-o-cara@LIVE.COM>
2021-12-24 16:57:38 -0300

    Configurado a classe context com o banco de dados SQL

---
+++ MinhasFinancas.Infra.Data/Configurations/Context.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MinhasFinancas.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhasFinancas.Infra.Data.Configurations
+{
+    public class Context : IdentityDbContext<User>
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringDeConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringDeConexao()
+        {
+            string strncon = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MINHAS_FINANCAS;Integrated Security=False;User ID=sa;Password=61403641;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            return strncon;
+        }
+    }
+}
